428b338b3e4aca498a5dfce2f1a883ff6453b076
Regy Saputra <45072445+regysaputra@users.noreply.github.com>
2022-12-02 21:51:21 +0700

    Initial commit

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { getMovies } from '../utils/api';
+
+function MoviesGrid() {
+  const [loading, setLoading] = React.useState(true);
+  const [movies, setMovies] = React.useState([]);
+
+  React.useEffect(() => {
+    getMovies().then((movies) => {
+      setMovies(movies);
+      setLoading(false);
+    });
+
+    return () => {
+      setLoading(true);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="movies-grid">
+        <p>Loading ....</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="movies-grid">
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id} className="movies-grid-item">
+            <img src={movie.poster} alt={movie.title} />
+            <div className="movie-grid-item--info">
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MoviesGrid;
